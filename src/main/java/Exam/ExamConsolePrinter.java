d5b48bd3e0b35970937391a60438c36268c95913
bilelfeki <bilel.feki.92@gmail.com>
2022-09-10 12:43:17 +0100

    Create a full Exam from the Console

---
+++ src/main/java/Exam/ExamConsolePrinter.java
@@ -0,0 +1,27 @@
+package Exam;
+
+import questions.QCM;
+
+public class ExamConsolePrinter {
+    private Exam exam = new Exam() ;
+
+    public void setExam(Exam exam) {
+        this.exam = exam;
+    }
+
+    public ExamConsolePrinter(){
+    }
+    public void read() {
+        exam.getNormalQuestionList()
+                .getNormalQuestionList()
+                .forEach(normalQuestion -> {
+                    System.out.println("yor Normal Question is");
+                    System.out.println(normalQuestion.getQuestionText());
+
+                });
+        exam.getQCMQuestionList().getAllQCM().forEach(QCM -> {
+            System.out.println("yor QCM Question is");
+            System.out.println(QCM.getQuestionText());
+        });
+    }
+}
--- src/main/java/questions/NormalQuestionList.java
+++ src/main/java/questions/NormalQuestionList.java
@@ -7,7 +7,17 @@ import java.util.List;
 public class NormalQuestionList {
     private List<NormalQuestion> normalQuestionList = new ArrayList<>() ;
 
+    public NormalQuestionList(){
+    }
+
     public NormalQuestionList(List<NormalQuestion> normalQuestionList) {
         this.normalQuestionList = normalQuestionList;
+    }
+    public void addNormalQuestion(NormalQuestion normalQuestion){
+        normalQuestionList.add(normalQuestion) ;
+    }
+
+    public List<NormalQuestion> getNormalQuestionList() {
+        return normalQuestionList;
     }
 }
